acb5cc9f2462303b5afed074dcd034c3b293b213
Jen-Chieh Shen <jcs090218@gmail.com>
2022-06-13 22:11:38 +0800

    add src

---
+++ sideline.el
@@ -0,0 +1,38 @@
+;;; sideline.el --- Show informations on the side  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Shen, Jen-Chieh
+;; Created date 2022-06-13 22:08:26
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Description: Show informations on the side
+;; Keyword: sideline
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "26.1"))
+;; URL: https://github.com/jcs-elpa/sideline
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Show informations on the side.
+;;
+
+;;; Code:
+
+
+
+(provide 'sideline)
+;;; sideline.el ends here
