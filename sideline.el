b4ada1ddf7da96c2e87453130f6ff7b7d577810f
JenChieh <jcs090218@gmail.com>
2026-01-01 13:40:43 +0800

    chore: Update copyright info

--- sideline.el
+++ sideline.el
@@ -1,6 +1,6 @@
 ;;; sideline.el --- Show information on the side  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2022-2025  Shen, Jen-Chieh
+;; Copyright (C) 2022-2026  Shen, Jen-Chieh
 ;; Created date 2022-06-13 22:08:26
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
